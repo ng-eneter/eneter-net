6680d4a7879b8ba04c9c2a8ccf12308dce47c244
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2014-03-02 19:46:50 +0000

    SyncDispatcher

---
+++ EneterMessaging/EneterMessagingFramework/DataProcessing/MessageQueueing/SingleThreadExecutor.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Project: Eneter.Messaging.Framework
+ * Author:  Ondrej Uzovic
+ * 
+ * Copyright © Ondrej Uzovic 2012
+*/
+
+using System;
+using System.Threading;
+using Eneter.Messaging.Diagnostic;
+
+namespace Eneter.Messaging.DataProcessing.MessageQueueing
+{
+    internal class SingleThreadExecutor
+    {
+        public SingleThreadExecutor()
+        {
+            myWorkingThread = new Thread(DoJobs);
+            myWorkingThread.IsBackground = true;
+            myWorkingThread.Start();
+
+            // Store the tracing info.
+            TracedObject = "TO ~ " + myWorkingThread.ManagedThreadId;
+        }
+
+        public void Execute(Action job)
+        {
+            EneterTrace.Debug(TracedObject);
+            myJobQueue.EnqueueMessage(job);
+        }
+
+        private void DoJobs()
+        {
+            while (true)
+            {
+                Action aJob = myJobQueue.DequeueMessage();
+
+                try
+                {
+                    // Execute the job.
+                    aJob();
+                }
+                catch (Exception err)
+                {
+                    EneterTrace.Error(TracedObject + ErrorHandler.DetectedException, err);
+                }
+            }
+        }
+
+        private MessageQueue<Action> myJobQueue = new MessageQueue<Action>();
+        private Thread myWorkingThread;
+        
+        private string TracedObject;
+    }
+}
