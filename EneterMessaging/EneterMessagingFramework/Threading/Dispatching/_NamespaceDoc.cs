5be0d31b0b27c04d77d32e0c52160905b837f3c7
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2014-03-09 09:46:09 +0000

    WinFormsDispatching added.

--- EneterMessaging/EneterMessagingFramework/Threading/Dispatching/_NamespaceDoc.cs
+++ EneterMessaging/EneterMessagingFramework/Threading/Dispatching/_NamespaceDoc.cs
@@ -13,7 +13,7 @@ namespace Eneter.Messaging.Threading.Dispatching
     /// Receiving messages and events according to specified thread mode. 
     /// </summary>
     /// <remarks>
-    /// Treading dispatching allows to specify in which threads received messages shall be received.  
+    /// Threading dispatching allows to specify in which threads received messages shall be received.  
     /// E.g. you can specify that events like messageReceived, connectionOpened, connectionClosed, ... will be raised in the main UI thread.
     /// </remarks>
     [CompilerGeneratedAttribute()]
