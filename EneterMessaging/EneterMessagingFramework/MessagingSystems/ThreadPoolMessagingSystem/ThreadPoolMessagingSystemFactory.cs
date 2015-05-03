22c2a1b26a3d31f880658c119fb5391700a3718f
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2015-05-03 12:02:19 +0000

    ThreadPoolMessaging doc updated.

--- EneterMessaging/EneterMessagingFramework/MessagingSystems/ThreadPoolMessagingSystem/ThreadPoolMessagingSystemFactory.cs
+++ EneterMessaging/EneterMessagingFramework/MessagingSystems/ThreadPoolMessagingSystem/ThreadPoolMessagingSystemFactory.cs
@@ -18,31 +18,22 @@ namespace Eneter.Messaging.MessagingSystems.ThreadPoolMessagingSystem
     /// Messaging system delivering messages asynchronously (when a message is received a separate thread is invoked to process it).
     /// </summary>
     /// <remarks>
-    /// The incoming messages are processed by multiple threads from the pool. When a message is received the thread
+    /// Each incoming message is routed into its own thread from the pool. It means when a message is received the thread
     /// from the pool is taken and the message is notified.
-    /// Therefore messages come asynchronously in various threads.
     /// </remarks>
     public class ThreadPoolMessagingSystemFactory : IMessagingSystemFactory
     {
         /// <summary>
-        /// Constructs the factory representing the messaging system.
+        /// Constructs the factory.
         /// </summary>
-        /// <remarks>
-        /// Every instance of the synchronous messaging system factory represents one messaging system.
-        /// It means that two instances of this factory class creates channels for two independent (differnt) messaging system.
-        /// </remarks>
         public ThreadPoolMessagingSystemFactory()
             : this(new LocalProtocolFormatter())
         {
         }
 
         /// <summary>
-        /// Constructs the factory representing the messaging system.
+        /// Constructs the factory.
         /// </summary>
-        /// <remarks>
-        /// Every instance of the synchronous messaging system factory represents one messaging system.
-        /// It means that two instances of this factory class creates channels for two independent (differnt) messaging system.
-        /// </remarks>
         /// <param name="protocolFormatter">formatter used to encode low-level messages between channels</param>
         public ThreadPoolMessagingSystemFactory(IProtocolFormatter protocolFormatter)
         {
@@ -56,15 +47,6 @@ namespace Eneter.Messaging.MessagingSystems.ThreadPoolMessagingSystem
         /// <summary>
         /// Creates the duplex output channel sending messages to the duplex input channel and receiving response messages by using the thread pool.
         /// </summary>
-        /// <remarks>
-        /// The duplex output channel is intended for the bidirectional communication.
-        /// Therefore, it can send messages to the duplex input channel and receive response messages.
-        /// <br/><br/>
-        /// The duplex input channel distinguishes duplex output channels according to the response receiver id.
-        /// This method generates the unique response receiver id automatically.
-        /// <br/><br/>
-        /// The duplex output channel can communicate only with the duplex input channel and not with the input channel.
-        /// </remarks>
         /// <param name="channelId">Identifies the receiving duplex input channel.</param>
         /// <returns>duplex output channel</returns>
         public IDuplexOutputChannel CreateDuplexOutputChannel(string channelId)
@@ -78,16 +60,6 @@ namespace Eneter.Messaging.MessagingSystems.ThreadPoolMessagingSystem
         /// <summary>
         /// Creates the duplex output channel sending messages to the duplex input channel and receiving response messages by using the thread pool.
         /// </summary>
-        /// <remarks>
-        /// The duplex output channel is intended for the bidirectional communication.
-        /// Therefore, it can send messages to the duplex input channel and receive response messages.
-        /// <br/><br/>
-        /// The duplex input channel distinguishes duplex output channels according to the response receiver id.
-        /// This method allows to specified a desired response receiver id. Please notice, the response receiver
-        /// id is supposed to be unique.
-        /// <br/><br/>
-        /// The duplex output channel can communicate only with the duplex input channel and not with the input channel.
-        /// </remarks>
         /// <param name="channelId">Identifies the receiving duplex input channel.</param>
         /// <param name="responseReceiverId">Identifies the response receiver of this duplex output channel.</param>
         /// <returns>duplex output channel</returns>
@@ -102,12 +74,6 @@ namespace Eneter.Messaging.MessagingSystems.ThreadPoolMessagingSystem
         /// <summary>
         /// Creates the duplex input channel receiving messages from the duplex output channel and sending back response messages by using the thread pool.
         /// </summary>
-        /// <remarks>
-        /// The duplex input channel is intended for the bidirectional communication.
-        /// It can receive messages from the duplex output channel and send back response messages.
-        /// <br/><br/>
-        /// The duplex input channel can communicate only with the duplex output channel and not with the output channel.
-        /// </remarks>
         /// <param name="channelId">Identifies this duplex input channel.</param>
         /// <returns>duplex input channel</returns>
         public IDuplexInputChannel CreateDuplexInputChannel(string channelId)
@@ -119,8 +85,7 @@ namespace Eneter.Messaging.MessagingSystems.ThreadPoolMessagingSystem
         }
 
         /// <summary>
-        /// Factory that will create dispatchers responsible for routing events from duplex input channel according to
-        /// desired threading strategy.
+        /// Threading mode for input channels.
         /// </summary>
         /// <remarks>
         /// Default setting is that all messages from all connected clients are routed by one working thread.
@@ -139,8 +104,7 @@ namespace Eneter.Messaging.MessagingSystems.ThreadPoolMessagingSystem
         }
 
         /// <summary>
-        /// Factory that will create dispatchers responsible for routing events from duplex output channel according to
-        /// desired threading strategy.
+        /// Threading mode for output channels.
         /// </summary>
         /// <remarks>
         /// Default setting is that received response messages are routed via one working thread.
