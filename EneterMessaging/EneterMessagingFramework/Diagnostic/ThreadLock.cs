2dd4e0926eaf280b424bda1e3e0c135c437472d9
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2015-08-27 21:22:35 +0000

    ThreadLock refactored + performance unit test added.

--- EneterMessaging/EneterMessagingFramework/Diagnostic/ThreadLock.cs
+++ EneterMessaging/EneterMessagingFramework/Diagnostic/ThreadLock.cs
@@ -7,7 +7,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading;
 
 namespace Eneter.Messaging.Diagnostic
@@ -16,144 +15,104 @@ namespace Eneter.Messaging.Diagnostic
     {
         private ThreadLock(object obj)
         {
-            if (IsEntered(obj))
-            {
-                //Monitor.Enter(obj);
-                return;
-            }
-
             myObj = obj;
-            
-            if (EneterTrace.DetailLevel == EneterTrace.EDetailLevel.Debug)
+        }
+
+        public static ThreadLock Lock(object obj)
+        {
+            ThreadLock aThreadLock;
+            lock (myActiveLocks)
             {
-                int aNumberOfWaitings;
-                lock (myWaitings)
+                myActiveLocks.TryGetValue(obj, out aThreadLock);
+
+                if (aThreadLock == null)
                 {
-                    myWaitings.TryGetValue(obj, out aNumberOfWaitings);
-                    ++aNumberOfWaitings;
-                    myWaitings[obj] = aNumberOfWaitings;
+                    aThreadLock = new ThreadLock(obj);
+                    myActiveLocks[obj] = aThreadLock;
+                }
+                else if (aThreadLock.OwningThread == Thread.CurrentThread)
+                {
+                    // If it is re-entering the lock.
+                    return null;
                 }
 
-                string[] aMessage = { "LOCKING ", aNumberOfWaitings.ToString() };
-                EneterTrace.Debug(1, string.Join("", aMessage));
+                ++aThreadLock.ReferenceCounter;
             }
 
+
+            aThreadLock.Lock();
+
+            return aThreadLock;
+        }
+
+        void IDisposable.Dispose()
+        {
+            Unlock();
+
+            lock (myActiveLocks)
+            {
+                --ReferenceCounter;
+                if (ReferenceCounter == 0)
+                {
+                    myActiveLocks.Remove(myObj);
+                }
+            }
+        }
+
+        private void Lock()
+        {
             DateTime aStartAcquiringTime = DateTime.Now;
 
+            // Wait until the lock is acquired.
             Monitor.Enter(myObj);
-
-#if !NET45
-            lock (myAcquiredLocks)
-            {
-                myAcquiredLocks[myObj] = Thread.CurrentThread;
-            }
-#endif
 
             DateTime aStopAcquiringTime = DateTime.Now;
             TimeSpan anElapsedTime = aStartAcquiringTime - aStartAcquiringTime;
 
+            OwningThread = Thread.CurrentThread;
+
             if (EneterTrace.DetailLevel == EneterTrace.EDetailLevel.Debug)
             {
-                lock (myWaitings)
-                {
-                    int aNumberOfWaitings;
-                    myWaitings.TryGetValue(obj, out aNumberOfWaitings);
-                    --aNumberOfWaitings;
-                    if (aNumberOfWaitings <= 0)
-                    {
-                        myWaitings.Remove(obj);
-                    }
-                    else
-                    {
-                        myWaitings[obj] = aNumberOfWaitings;
-                    }
-                }
-
                 string[] aMessage = { "LOCKED ", anElapsedTime.ToString() };
-                EneterTrace.Debug(1, string.Join("", aMessage));
+                EneterTrace.Debug(2, string.Join("", aMessage));
             }
 
             if (anElapsedTime >= TimeSpan.FromMilliseconds(1000))
             {
                 string[] aMessage = { "Locked after [ms]: ", anElapsedTime.TotalMilliseconds.ToString() };
-                EneterTrace.Warning(1, string.Join("", aMessage));
+                EneterTrace.Warning(2, string.Join("", aMessage));
             }
 
             myLockTime = aStopAcquiringTime;
         }
 
-        public static ThreadLock Lock(object obj)
+        private void Unlock()
         {
-            return new ThreadLock(obj);
-        }
+            OwningThread = null;
 
-        public void Dispose()
-        {
-            if (myObj == null)
-            {
-                return;
-            }
-
-#if !NET45
-            lock (myAcquiredLocks)
-            {
-                myAcquiredLocks.Remove(myObj);
-            }
-#endif
-
+            // Release the lock.
             Monitor.Exit(myObj);
 
-            if (myObj != null)
+            TimeSpan anElapsedTime = DateTime.Now - myLockTime;
+
+            if (EneterTrace.DetailLevel == EneterTrace.EDetailLevel.Debug)
             {
-                TimeSpan anElapsedTime = DateTime.Now - myLockTime;
-
-                if (EneterTrace.DetailLevel == EneterTrace.EDetailLevel.Debug)
-                {
-                    string[] aMessage = { "UNLOCKED ", anElapsedTime.ToString() };
-                    EneterTrace.Debug(1, string.Join("", aMessage));
-                }
-                if (anElapsedTime >= TimeSpan.FromMilliseconds(1000))
-                {
-                    string[] aMessage = { "Unlocked after [ms]: ", anElapsedTime.TotalMilliseconds.ToString() };
-                    EneterTrace.Warning(1, string.Join("", aMessage));
-                }
-
-
+                string[] aMessage = { "UNLOCKED ", anElapsedTime.ToString() };
+                EneterTrace.Debug(2, string.Join("", aMessage));
+            }
+            if (anElapsedTime >= TimeSpan.FromMilliseconds(1000))
+            {
+                string[] aMessage = { "Unlocked after [ms]: ", anElapsedTime.TotalMilliseconds.ToString() };
+                EneterTrace.Warning(2, string.Join("", aMessage));
             }
         }
 
-
-        private bool IsEntered(object obj)
-        {
-#if NET45
-            return Monitor.IsEntered(obj);
-#else
-            Thread aThread;
-
-            lock (myAcquiredLocks)
-            {
-                myAcquiredLocks.TryGetValue(obj, out aThread);
-            }
-
-            if (aThread != null)
-            {
-                if (Thread.CurrentThread == aThread)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-#endif
-        }
-
+        private Thread OwningThread { get; set; }
+        private int ReferenceCounter { get; set; }
 
         private DateTime myLockTime;
         private object myObj;
-        private static Dictionary<object, int> myWaitings = new Dictionary<object, int>();
 
-#if !NET45
-        private static Dictionary<object, Thread> myAcquiredLocks = new Dictionary<object, Thread>();
-#endif
+        private static Dictionary<object, ThreadLock> myActiveLocks = new Dictionary<object, ThreadLock>();
     }
 }
---
+++ EneterMessaging/MessagingUnitTests/Diagnostic/Test_ThreadLock.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading;
+using Eneter.Messaging.Diagnostic;
+
+namespace Eneter.MessagingUnitTests.Diagnostic
+{
+    [TestFixture]
+    public class Test_ThreadLock
+    {
+        [Test]
+        public void PerformanceTest()
+        {
+            PerformanceTimer aTimer = new PerformanceTimer();
+
+            ManualResetEvent aCompleted = new ManualResetEvent(false);
+            object aLock = new object();
+            int aSum = 0;
+
+
+            // Using synchronized keyword
+            aTimer.Start();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                ThreadPool.QueueUserWorkItem(x =>
+                {
+                    for (int j = 0; j < 1000000; ++j)
+                    {
+                        lock (aLock)
+                        {
+                            ++aSum;
+                            if (aSum == 10000000)
+                            {
+                                aCompleted.Set();
+                            }
+                        }
+                    }
+                });
+            }
+
+            aCompleted.WaitOne();
+            aTimer.Stop();
+
+
+            // Using ThreadLock
+            aCompleted.Reset();
+            aTimer.Start();
+            aSum = 0;
+
+            for (int i = 0; i < 10; ++i)
+            {
+                ThreadPool.QueueUserWorkItem(x =>
+                {
+                    for (int j = 0; j < 1000000; ++j)
+                    {
+                        using (ThreadLock.Lock(aLock))
+                        {
+                            ++aSum;
+                            if (aSum == 10000000)
+                            {
+                                aCompleted.Set();
+                            }
+                        }
+                    }
+                });
+            }
+
+            aCompleted.WaitOne();
+            aTimer.Stop();
+        }
+
+    }
+}
