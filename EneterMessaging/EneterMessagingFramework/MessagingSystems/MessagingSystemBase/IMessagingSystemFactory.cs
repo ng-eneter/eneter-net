1683daffec597596767977248aa3106c6ee747b1
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2015-11-14 12:13:34 +0000

    IMessagingSystemFactory documenation updated.

--- EneterMessaging/EneterMessagingFramework/MessagingSystems/MessagingSystemBase/IMessagingSystemFactory.cs
+++ EneterMessaging/EneterMessagingFramework/MessagingSystems/MessagingSystemBase/IMessagingSystemFactory.cs
@@ -12,7 +12,7 @@ using Eneter.Messaging.MessagingSystems.ConnectionProtocols;
 namespace Eneter.Messaging.MessagingSystems.MessagingSystemBase
 {
     /// <summary>
-    /// Messaging factory to create output and input channels.
+    /// Creates output and input channels.
     /// </summary>
     /// <remarks>
     /// This factory interface is supposed to be implemented by all messaging systems.
@@ -24,22 +24,23 @@ namespace Eneter.Messaging.MessagingSystems.MessagingSystemBase
         /// <summary>
         /// Creates the duplex output channel that sends messages to the duplex input channel and receives response messages.
         /// </summary>
-        /// <param name="channelId">id representing receiving input channel address.</param>
+        /// <param name="channelId">address of the input channel</param>
         /// <returns>duplex output channel</returns>
         IDuplexOutputChannel CreateDuplexOutputChannel(string channelId);
 
         /// <summary>
         /// Creates the duplex output channel that sends messages to the duplex input channel and receives response messages.
         /// </summary>
-        /// <param name="channelId">id representing receiving input channel address.</param>
-        /// <param name="responseReceiverId">unique identifier of the response receiver represented by this duplex output channel.</param>
+        /// <param name="channelId">address of the input channel</param>
+        /// <param name="responseReceiverId">unique identifier of this duplex output channel. If the value is null then
+        /// the identifier is genearated automatically</param>
         /// <returns>duplex output channel</returns>
         IDuplexOutputChannel CreateDuplexOutputChannel(string channelId, string responseReceiverId);
 
         /// <summary>
         /// Creates the duplex input channel that receives messages from the duplex output channel and sends back response messages.
         /// </summary>
-        /// <param name="channelId">id representing the input channel address.</param>
+        /// <param name="channelId">address on which the duplex input channel shall listen</param>
         /// <returns>duplex input channel</returns>
         IDuplexInputChannel CreateDuplexInputChannel(string channelId);
     }
