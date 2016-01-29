884f170ac54155f52fae896cb5dd8bd70bb3fbc5
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2016-01-29 22:01:54 +0000

    ScalableThreadPool and EneterTimer added.

---
+++ EneterMessaging/EneterMessagingFramework/Threading/EneterTimer.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Project: Eneter.Messaging.Framework
+ * Author:  Ondrej Uzovic
+ * 
+ * Copyright © Ondrej Uzovic 2016
+*/
+
+using System.Threading;
+
+namespace Eneter.Messaging.Threading
+{
+	internal class EneterTimer
+	{
+		public EneterTimer(TimerCallback callback)
+		{
+            myTickCallback = callback;
+		}
+
+		public void Change(int millisecondsTimeout)
+		{
+            //using (EneterTrace.Entering())
+            //{
+                lock (myScheduleLock)
+                {
+                    // Release the previous waiting.
+                    myTimeElapsedEvent.Set();
+
+                    if (millisecondsTimeout > -1)
+                    {
+                        // Set the new waiting.
+                        myMillisecondsTimeout = millisecondsTimeout;
+                        myWaitingThreadPool.Execute(DoWaiting);
+
+                        // Wait until the waiting started.
+                        // This is to avoid mulitple waitings cumulated the queue.
+                        myWaitingStartedEvent.WaitOne(5000);
+                    }
+                }
+            //}
+		}
+
+		private void DoWaiting()
+		{
+            //using (EneterTrace.Entering())
+            //{
+                // Indicate the waiting started.
+                myWaitingStartedEvent.Set();
+
+                // Wait until the time elapses or until the waiting is canceled.
+                myTimeElapsedEvent.Reset();
+                if (myTimeElapsedEvent.WaitOne(myMillisecondsTimeout))
+                {
+                    // if the waiting is canceled then just return.
+                    return;
+                }
+
+                // The time ellapsed so call the tick callback.
+                // Execute it in a different thread so that if the tick-callback calls EneterTimer.Change
+                // the deadlock will not happen.
+                myTickingThreadPool.Execute(() => myTickCallback(null));
+            //}
+		}
+
+		private TimerCallback myTickCallback;
+		private int myMillisecondsTimeout;
+        private ManualResetEvent myTimeElapsedEvent = new ManualResetEvent(false);
+        private AutoResetEvent myWaitingStartedEvent = new AutoResetEvent(false);
+        private object myScheduleLock = new object();
+        private ScalableThreadPool myWaitingThreadPool = new ScalableThreadPool(0, 1, 1000);
+        private ScalableThreadPool myTickingThreadPool = new ScalableThreadPool(0, 100, 10000);
+
+	}
+}
---
+++ EneterMessaging/EneterMessagingFramework/Threading/ScalableThreadPool.cs
@@ -0,0 +1,170 @@
+﻿/*
+ * Project: Eneter.Messaging.Framework
+ * Author:  Ondrej Uzovic
+ * 
+ * Copyright © Ondrej Uzovic 2016
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Eneter.Messaging.Threading
+{
+    class ScalableThreadPool
+    {
+        // Synchronized queue for tasks.
+        private class TaskQueue
+        {
+            public bool Enqueue(Action task)
+            {
+                //using (EneterTrace.Entering())
+                //{
+                    // Wait until lock is acquired.
+                    lock (myTasks)
+                    {
+                        // Put task to the queue.
+                        myTasks.Enqueue(task);
+
+                        // Check if there is a thread(s) waiting until a new task is entered.
+                        bool anIdleThreadExist = myNumberOfIdleThreads > 0;
+
+                        // Release the lock and signal the new task was entered.
+                        Monitor.Pulse(myTasks);
+
+                        return anIdleThreadExist;
+                    }
+                //}
+            }
+
+            public Action Dequeue(int timeout)
+            {
+                //using (EneterTrace.Entering())
+                //{
+                    // Wait until lock is acquired.
+                    lock (myTasks)
+                    {
+                        // If the queue with task is not empty then remove the first one and process it.
+                        if (myTasks.Count > 0)
+                        {
+                            return myTasks.Dequeue();
+                        }
+
+                        // The queue with tasks is empty so return the lock and wait until
+                        // a task is entered and the lock is acquired again or until timeout.
+                        ++myNumberOfIdleThreads;
+                        if (!Monitor.Wait(myTasks, timeout))
+                        {
+                            // If the lock is not reacquired.
+                            --myNumberOfIdleThreads;
+                            return null;
+                        }
+
+                        // The lock is reacquired. It means something is in the queue.
+                        --myNumberOfIdleThreads;
+                        return myTasks.Dequeue();
+                    }
+                //}
+            }
+
+
+            private int myNumberOfIdleThreads;
+            private Queue<Action> myTasks = new Queue<Action>();
+        }
+
+        // Thread living in the pool.
+        private class PoolThread
+        {
+            public PoolThread(ScalableThreadPool threadPool)
+            {
+                myThreadPool = threadPool;
+                myThread = new Thread(Run);
+                myThread.IsBackground = true;
+				myThread.Name = threadPool.myThreadsName;
+            }
+
+            public void start()
+            {
+                myThread.Start();
+            }
+
+            private void Run()
+            {
+                while (true)
+                {
+                    Action aTask = myThreadPool.myTaskQueue.Dequeue(myThreadPool.myMaxIdleTime);
+                    if (aTask != null)
+                    {
+                        try
+                        {
+                            aTask();
+                        }
+                        catch
+                        {
+                            // If task throws an exception then do not care beacause the thread pool needs to continue.
+                        }
+                    }
+                    else
+                    {
+                        lock (myThreadPool.myNumberOfThreadsManipulator)
+                        {
+                            if (myThreadPool.myNumberOfThreads > myThreadPool.myMinNumberOfThreads)
+                            {
+                                --myThreadPool.myNumberOfThreads;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            private ScalableThreadPool myThreadPool;
+            private Thread myThread;
+        }
+
+		public ScalableThreadPool(int minThreads, int maxThreads, int maxIdleTime)
+			: this(minThreads, maxThreads, maxIdleTime, "ScalableThreadPool")
+		{
+		}
+
+		public ScalableThreadPool(int minThreads, int maxThreads, int maxIdleTime, string threadsName)
+        {
+            myMinNumberOfThreads = minThreads;
+            myMaxNumberOfThreads = maxThreads;
+            myMaxIdleTime = maxIdleTime;
+			myThreadsName = threadsName;
+        }
+
+        public void Execute(Action task)
+        {
+            // Enqueue the task.
+            bool anIdleThreadExist = myTaskQueue.Enqueue(task);
+
+            // If there is no free idle thread then start one if it does not exceed number of maximum threads.
+            if (!anIdleThreadExist)
+            {
+                lock (myNumberOfThreadsManipulator)
+                {
+                    if (myNumberOfThreads < myMaxNumberOfThreads)
+                    {
+                        ++myNumberOfThreads;
+
+                        PoolThread aThread = new PoolThread(this);
+                        aThread.start();
+                    }
+                }
+            }
+        }
+
+
+        private int myMinNumberOfThreads;
+        private int myMaxNumberOfThreads;
+        private int myMaxIdleTime;
+		private string myThreadsName;
+
+        private Object myNumberOfThreadsManipulator = new Object();
+        private int myNumberOfThreads;
+
+        private TaskQueue myTaskQueue = new TaskQueue();
+    }
+}
