6cab1041f6412def689359d751261636b8d9e620
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2019-11-18 12:09:11 +0100

    Version changed to 8.0.5

--- EneterMessaging/EneterMessagingFramework/Properties/AssemblyInfo.cs
+++ EneterMessaging/EneterMessagingFramework/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.0.4.0")]
-[assembly: AssemblyFileVersion("8.0.4.0")]
+[assembly: AssemblyVersion("8.0.5.0")]
+[assembly: AssemblyFileVersion("8.0.5.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
 
 [assembly: InternalsVisibleTo("Eneter.MessagingUnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d577cc6b8dc7bfe3236c3864ea05cebee8062b64ac9d39d8bd2fd17e732806dd6102586f7d39264f6521a096482fcdf3c629d12031f8481fb767bc2816d43cf8006923c3d9282d8c6e5b7dcb1228f852323b176eb97aec600c8bfee59522a1daeddee55731fb1c7c3258b5fec7c35cb2510778668ae353405533235f684734bc")]
