d76443648185843eabb83a2251e2de4d2bbfff11
Ondrej Uzovic <ondrej.uzovic@gmail.com>
2020-04-07 12:49:56 +0200

    List extension RemoveWhere(..) added.

---
+++ EneterMessaging/EneterMessagingFramework/Utils/Collections/ListExt.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Project: Eneter.Messaging.Framework
+ * Author:  Ondrej Uzovic
+ * 
+ * Copyright (c) Ondrej Uzovic 2020
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Eneter.Messaging.Utils.Collections
+{
+    internal static class ListExt
+    {
+        public static int RemoveWhere<T>(this IList<T> list,  Predicate<T> match)
+        {
+            int count = 0;
+            for (int i = list.Count - 1; i >= 0; --i)
+            {
+                T item = list[i];
+                if (match(item))
+                {
+                    list.RemoveAt(i);
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ EneterMessaging/MessagingUnitTests/Utils/Collections/Test_ListExt.cs
@@ -0,0 +1,22 @@
+﻿using Eneter.Messaging.Utils.Collections;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Eneter.MessagingUnitTests.Utils.Collections
+{
+    [TestFixture]
+    public class Test_ListExt
+    {
+        [Test]
+        public void RemoveWhere()
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+            int count = list.RemoveWhere(x => x < 4);
+
+            Assert.AreEqual(3, count);
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual(4, list[0]);
+            Assert.AreEqual(5, list[1]);
+        }
+    }
+}
